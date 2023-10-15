d2821c33c003f84926774b11a9576750f91aa23e
alex <a.oldemonnikhof@gmail.com>
2023-10-15 17:43:52 +0100

    ex05 completed

---
+++ cpp_01/ex05/src/main.cpp
@@ -0,0 +1,12 @@
+#include "../inc/Harl.hpp"
+
+int	main(void)
+{
+	Harl	harl1;
+
+	harl1.complain("DEBUG");
+	// harl1.complain("INFO");
+	// harl1.complain("WARNING");
+	// harl1.complain("ERROR");
+	return (0);
+}
