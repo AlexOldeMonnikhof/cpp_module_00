ef78394e93006c1641b6f1ee3b9acf3625bcec44
Alex Olde Monnikhof <aolde-mo@e2r7p7.1337.ma>
2023-10-30 13:19:22 +0100

    added more tests

--- cpp_03/ex00/src/main.cpp
+++ cpp_03/ex00/src/main.cpp
@@ -9,8 +9,13 @@ int	main()
 	c1.takeDamage(5);
 	c1.takeDamage(5);
 	c1.takeDamage(5);
+
 	c2.takeDamage(4);
+
+	// to show that energy points work
 	for (size_t i = 0; i < 11; i++)
 		c2.beRepaired(1);
+
+	c1.attack("joe");
 	return (0);
 }
