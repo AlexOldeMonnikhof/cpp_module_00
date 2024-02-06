90aac59a0cd647dce65c3cf0539e6bb4150cd053
Alex Olde Monnikhof <a.oldemonnikhof@gmail.com>
2024-02-06 14:10:54 +0100

    files ex01 added

---
+++ cpp_05/ex01/src/main.cpp
@@ -0,0 +1,28 @@
+#include "../inc/Bureaucrat.hpp"
+
+int main()
+{
+	try
+	{
+		Bureaucrat john("john");
+		Bureaucrat bob("bob", 59);
+		Bureaucrat larry("larry", 1);
+
+		//decrementing grade
+		for (size_t i = 0; i < 100; i++)
+			larry.decrementGrade();
+		std::cout << larry << std::endl;
+
+		// //error: grade too low
+		Bureaucrat peter("peter", 151);
+
+		//error: grade too high
+		Bureaucrat mitchell("mitchell", 1);
+		mitchell.incrementGrade();
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
+}
