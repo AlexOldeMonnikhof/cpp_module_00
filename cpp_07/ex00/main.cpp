0fb8a334b2aa0b54fd3d777b22c31c577e9df7fa
Alex Olde Monnikhof <a.oldemonnikhof@gmail.com>
2024-08-18 17:01:27 +0100

    added files

---
+++ cpp_07/ex00/main.cpp
@@ -0,0 +1,16 @@
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Error: invalid amount of arguments" << std::endl;
+        return (1);
+    }
+    if (!argv[1][0])
+    {
+        std::cerr << "Error: empty argument" << std::endl;
+        return (1);
+    }
+    ScalarConverter::convert((const std::string)argv[1]);
+    return (0);
+}
