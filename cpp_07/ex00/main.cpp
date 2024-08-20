4f74d4fa57e74075996c9fc9a5a58176350afd45
Alex Olde Monnikhof <a.oldemonnikhof@gmail.com>
2024-08-20 21:03:43 +0100

    ex00 completed

--- cpp_07/ex00/main.cpp
+++ cpp_07/ex00/main.cpp
@@ -1,16 +1,18 @@
+#include "whatever.hpp"
 
-int main(int argc, char **argv)
+int main()
 {
-    if (argc != 2)
-    {
-        std::cerr << "Error: invalid amount of arguments" << std::endl;
-        return (1);
-    }
-    if (!argv[1][0])
-    {
-        std::cerr << "Error: empty argument" << std::endl;
-        return (1);
-    }
-    ScalarConverter::convert((const std::string)argv[1]);
-    return (0);
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
 }
