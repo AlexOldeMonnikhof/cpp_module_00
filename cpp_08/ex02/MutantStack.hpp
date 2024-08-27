4819e1354ec9254d6d0fd0741625156f3530f176
alex <a.oldemonnikhof@gmail.com>
2024-08-28 00:18:15 +0100

    ex02 completed

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -2,13 +2,37 @@
 # define MUTANTSTACK_HPP
 
 # include <iostream>
+# include <stack>
+# include <iostream>
 
 template <typename T>
-class MutantStack
+class MutantStack : public std::stack<T>
 {
+    public:
+        MutantStack(){};
+        MutantStack(const MutantStack& other)
+        {
+            *this = other;
+        };
 
+        ~MutantStack(){};
+
+        MutantStack& operator=(const MutantStack& other)
+        {
+            if (this != &other)
+                this.c = other.c;
+            return (*this);
+        };
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator	begin()
+        {
+            return (this->c.begin());
+        }
+        iterator	end()
+        {
+            return (this->c.end());
+        }
 };
-
-# include "MutantStack.tpp"
 
 #endif
--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -7,10 +7,15 @@ int main()
     mstack.push(5);
     mstack.push(17);
 
-    std::cout << mstack.top() << std::endl;
 
+    std::cout << "stack = 5, 17" << std::endl;
+
+    std::cout << "stack top = " << mstack.top() << std::endl;
+
+    std::cout << "popping top" << std::endl;
     mstack.pop();
 
+    std::cout << "size = ";
     std::cout << mstack.size() << std::endl;
 
     mstack.push(3);
@@ -19,17 +24,17 @@ int main()
     //[...]
     mstack.push(0);
 
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
+    MutantStack<int>::iterator iter = mstack.begin();
+    MutantStack<int>::iterator iterEnd = mstack.end();
     
-    ++it;
-    --it;
-    while (it != ite)
+    ++iter;
+    --iter;
+    std::cout << "stack = ";
+    while (iter != iterEnd)
     {
-        std::cout << *it << std::endl;
-        ++it;
+        std::cout << *iter << ", ";
+        ++iter;
     }
     std::stack<int> s(mstack);
     return 0;
 }
-
