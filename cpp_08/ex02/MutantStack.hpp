d5e1c185a7ea1bd0ffbdca6e6830608e5f5399e1
alex <a.oldemonnikhof@gmail.com>
2024-08-28 01:12:37 +0100

    ex02 completed

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -4,8 +4,11 @@
 # include <iostream>
 # include <stack>
 # include <iostream>
+# include <list>
 
 template <typename T>
+//std::stack by default uses std::deque as container type
+//so no need to specify it like std::stack<T, std::deque<T>>
 class MutantStack : public std::stack<T>
 {
     public:
@@ -24,11 +27,14 @@ class MutantStack : public std::stack<T>
             return (*this);
         };
 
+        //container_type = std::deque so could also define as std::deque<T>::iterator
+        //using container_type is more flexible > will work when changing underlying container
         typedef typename std::stack<T>::container_type::iterator iterator;
         iterator	begin()
         {
             return (this->c.begin());
         }
+        
         iterator	end()
         {
             return (this->c.end());
--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -2,39 +2,91 @@
 
 int main()
 {
-    MutantStack<int> mstack;
-
-    mstack.push(5);
-    mstack.push(17);
-
-
-    std::cout << "stack = 5, 17" << std::endl;
-
-    std::cout << "stack top = " << mstack.top() << std::endl;
-
-    std::cout << "popping top" << std::endl;
-    mstack.pop();
-
-    std::cout << "size = ";
-    std::cout << mstack.size() << std::endl;
-
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-
-    MutantStack<int>::iterator iter = mstack.begin();
-    MutantStack<int>::iterator iterEnd = mstack.end();
-    
-    ++iter;
-    --iter;
-    std::cout << "stack = ";
-    while (iter != iterEnd)
+    std::cout << "------------STACK-----------" << std::endl;
     {
-        std::cout << *iter << ", ";
+        MutantStack<int> mstack;
+
+        mstack.push(5);
+        mstack.push(17);
+
+
+        std::cout << "stack = 5, 17" << std::endl;
+        std::cout << "stack top = " << mstack.top() << std::endl;
+
+        std::cout << "popping top" << std::endl;
+        mstack.pop();
+
+        std::cout << "size = ";
+        std::cout << mstack.size() << std::endl;
+
+        std::cout << "pushing 3, 5, 737, 0" << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        //[...]
+        mstack.push(0);
+
+        MutantStack<int>::iterator iter = mstack.begin();
+        MutantStack<int>::iterator iterEnd = mstack.end();
+
         ++iter;
+        --iter;
+        std::cout << "stack = ";
+        while (iter != iterEnd)
+        {
+            if (iter == iterEnd - 1)
+                std::cout << *iter << std::endl;
+            else
+                std::cout << *iter << ", ";
+            ++iter;
+        }
+
+        std::stack<int> s(mstack);
     }
-    std::stack<int> s(mstack);
+    std::cout << "------------LIST------------" << std::endl;
+    {
+        //stack: push, pop
+        //list: push_back, pop_back
+
+        std::list<std::string> mlist;
+
+        mlist.push_back("bob");
+        mlist.push_back("john");
+
+
+        std::cout << "stack = bob, jonn" << std::endl;
+        std::cout << "stack top = " << mlist.back() << std::endl;
+
+        std::cout << "popping top" << std::endl;
+        mlist.pop_back();
+
+        std::cout << "size = ";
+        std::cout << mlist.size() << std::endl;
+
+        std::cout << "pushing tom, peter, rick, max" << std::endl;
+        mlist.push_back("tom");
+        mlist.push_back("peter");
+        mlist.push_back("rick");
+        //[...]
+        mlist.push_back("max");
+
+        //no need to create our own end() or begin()
+        std::list<std::string>::iterator iter = mlist.begin();
+
+        ++iter;
+        --iter;
+
+        std::cout << "stack = ";
+        while (iter != mlist.end())
+        {
+            if (iter == --mlist.end())
+                std::cout << *iter << std::endl;
+            else
+                std::cout << *iter << ", ";
+            ++iter;
+        }
+
+        std::list<std::string> s(mlist);
+    }   
     return 0;
 }
