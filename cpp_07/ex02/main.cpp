7af362731338be730011f5af8787360807cec3d0
Alex Olde Monnikhof <a.oldemonnikhof@gmail.com>
2024-08-29 18:23:43 +0100

    improved main

--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -1,5 +1,7 @@
 #include "Array.hpp"
 
+#define MAX_VAL 750
+
 int main()
 {
     std::cout << "integer array:" << std::endl;
@@ -65,6 +67,60 @@ int main()
     catch (std::exception& e)
     {
         std::cerr << e.what() << std::endl;
+    }
+    std::cout << "\nsubject:" << std::endl;
+    {
+        Array<int> numbers(MAX_VAL);
+        int* mirror = new int[MAX_VAL];
+        srand(time(NULL));
+        for (int i = 0; i < MAX_VAL; i++)
+        {
+            const int value = rand();
+            numbers[i] = value;
+            mirror[i] = value;
+        }
+        //SCOPE
+        {
+            Array<int> tmp = numbers;
+            Array<int> test(tmp);
+
+            //loop through test:
+            for (int i = 0; i < 5; i++)
+                std::cout << "numbers: " << numbers[i] << "\ttest: " << test[i] << std::endl;
+            
+        }
+
+        for (int i = 0; i < MAX_VAL; i++)
+        {
+            if (mirror[i] != numbers[i])
+            {
+                std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+        }
+        try
+        {
+            numbers[-2] = 0;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        try
+        {
+            numbers[MAX_VAL] = 0;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        std::cout << "changing numbers values again..." << std::endl;
+        for (int i = 0; i < 5; i++)
+        {
+            numbers[i] = rand();
+            std::cout << "numbers: " << numbers[i] << std::endl;
+        }
+        delete [] mirror;//
     }
     return 0;
 }
